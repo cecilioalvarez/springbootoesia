9adf240448ca2efb5eb2a58a48d47b2d05c9a3e6
cecilioAlvarez <cmac@arquitecturajava.com>
2023-11-13 13:27:47 +0100

    mock y verificacion

--- springtesting1/src/test/java/es/oesia/springtesting1/SpringPersonaServiceTest.java
+++ springtesting1/src/test/java/es/oesia/springtesting1/SpringPersonaServiceTest.java
@@ -2,13 +2,15 @@ package es.oesia.springtesting1;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
@@ -19,22 +21,33 @@ class SpringPersonaServiceTest {
 	
 	@Autowired
 	PersonaService repoPersonaService;
+	@MockBean
+	PersonaRepository repoPersonaMock;
 	
 	
 	@Test
 
 	void getPersonasTest() {
 		
-		List<Persona> lista=repoPersonaService.getPersonas();
+		//when(repoPersonaMock.getPersonas()).thenReturn(List.of(new Persona("ana"),new Persona("juan")));
+		
+		
+		List<Persona> lista= repoPersonaService.getPersonas();
 		assertEquals(2,lista.size());
 	}
 	
 	@Test
 	void addPersonaTest() {
 		
+		//persona service add
+		//porque es un metodo delegado
+		// al ser un metodo delegado no hace nada
+		// entonces verificar aqui que hemos añadido una persona
+		// con las listas no tiene sentido
+		//porque eso lo hace el repo
+		
 		repoPersonaService.add(new Persona("ana","perez",40));
-		List<Persona> lista=repoPersonaService.getPersonas();
-		assertTrue(lista.contains(new Persona("ana")));
+	    Mockito.verify(repoPersonaMock).add(new Persona("ana","perez",40));
 	}
 	
 	@Test
